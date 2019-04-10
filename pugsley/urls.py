584a651351cb4ba8b247cf880db9b00cde8dda6e
Kurtis Fields <kurtisfields@gmail.com>
2019-04-10 02:00:43 -0400

    calendar stuff

--- pugsley/urls.py
+++ pugsley/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('avatar/', include('avatar.urls')),
+    re_path(r'^schedule/', include('schedule.urls')),
 
     re_path(r'', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/calendar.js
+++ src/calendar.js
@@ -6,13 +6,28 @@ import '@fullcalendar/daygrid/main.css';
 // import '@fullcalendar/timegrid/main.css';
 // import '@fullcalendar/list/main.css';
 
+import $ from 'jquery';
+const csrftoken = $("[name=csrfmiddlewaretoken]").val();
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
 document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+  });
+  
+  const calendarEl = document.getElementById('calendar');
 
-  var calendar = new Calendar(calendarEl, {
+  const calendar = new Calendar(calendarEl, {
+    events: '/schedule/api/occurrences?calendar_slug=pugsley',
     plugins: [ dayGridPlugin ],
     defaultView: 'dayGridMonth'
   });
-
+  
   calendar.render();
 });
