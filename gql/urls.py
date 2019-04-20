e5b18fe990feb1192e2185a5484bccbd63716dcc
Kurtis Fields <kurtisfields@gmail.com>
2019-04-20 11:42:04 -0400

    playground csrf fix

--- gql/urls.py
+++ gql/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 from graphene_django.views import GraphQLView
+#TODO:  Isn't there a way to use csrf?  Why was GrapiQl working and not Playground?
+from django.views.decorators.csrf import csrf_exempt
+
+class MyGraphQLView(GraphQLView):
+    def __init__(self, *args, **kwargs):
+        self.graphiql_template = "gql/playground.html"
+        super(MyGraphQLView, self).__init__(*args, **kwargs)
 
 urlpatterns = [
-    path('', GraphQLView.as_view(graphiql=True)),
+    # path('', GraphQLView.as_view(graphiql=True)),
+    path('', csrf_exempt(MyGraphQLView.as_view(graphiql=True))),
 ]
