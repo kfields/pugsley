aa0570c5cdad8454b32baccac0efc35bcd0ea4ee
Kurtis Fields <kurtisfields@gmail.com>
2019-04-22 11:38:15 -0400

    add redirect for mc

--- pugsley/urls.py
+++ pugsley/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -21,5 +22,7 @@ urlpatterns = [
     path('avatar/', include('avatar.urls')),
     path('schedule/', include('schedule.urls')),
     path('graphql/', include('gql.urls')),
+
+    path('mc/', RedirectView.as_view(url='/static/mc/index.html'), name='mc'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
